17ea7e99f42e64cb1ced891284f35736871c1b05
Dkw-69 <filenotfound6@gmail.com>
2021-01-22 15:11:17 +0300

    deleted fluff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
-    author: {
-      name: `David K.`,
-      summary: `who lives and works in the UK building useful things.`,
-    },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
   },
